9f807c17574e72abd5a07fa5930aefe08558552c
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-09 19:28:21 -0500

    working python

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,30 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
+# sequence = [4, 3, 5, 0, 1]
+# swaps = 0
 
-# Your Code Here
+def bubblesort(sequence):
+    swapped = False
+    # Looping from size of array from last index[-1] to index [0]
+    for k in range(len(sequence)-1, 0, -1):
+        for j in range(k):
+            if sequence[j] > sequence[j + 1]:
+                swapped = True
+                # swapping data if the element is less than next element in the array
+                sequence[j], sequence[j + 1] = sequence[j + 1], sequence[j]
+                print(sequence)	
+            if not swapped:
+                print(sequence)
+                # exiting the function if we didn't make a single swap
+                # meaning that the array is already sorted.
+                return
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+sequence = [39, 12, 18, 85, 72, 10, 2, 18]
+
+# print("Unsorted list is,")
+# print(sequence)
+print(bubblesort(sequence))
+# print("Sorted Array is, ")
+# print(sequence)
+
+
+# print(f"Final result: {result}")
+# print(f"Swaps: {swaps}")
